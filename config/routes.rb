bd0d7adeda9923f30ee038e76330cf1b51e6f31e
EC2 Default User <ec2-user@ip-172-31-0-179.ec2.internal>
2022-03-01 09:43:53 +0000

    homesコントローラーとtop,about view作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get 'homes/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
