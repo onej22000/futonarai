10db1997eb6b506a4058e67271a27d67c30e60b5
EC2 Default User <ec2-user@ip-172-31-0-179.ec2.internal>
2022-03-04 04:58:33 +0000

    serviceモデル、コントローラー、view、ActiveStrage作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
   root to: 'homes#top'
   get '/about' => 'homes#about'
+  devise_for :customers
+  devise_for :admin
+
+  namespace :customer do
+    resources :service, only: [:index, :show]
+  end
+
+  namespace :admin do
+    resources :service
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
