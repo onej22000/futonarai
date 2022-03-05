8d08d736a1c3b4e594025c487fda252fcd3c2fef
EC2 Default User <ec2-user@ip-172-31-0-179.ec2.internal>
2022-03-05 12:05:49 +0000

    admin/cusetomer/editとshowのエラー解消

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -10,4 +10,18 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email, :family_name, :first_name,:kana_family_name, :kana_first_name, :postal_code, :address, :tel, :status )
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :service
-    resources :customers, only: [:index, :show, :edit]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
