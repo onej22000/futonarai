4a8f32ec9f995dffe7832fcc9ad516966a209eff
EC2 Default User <ec2-user@ip-172-31-0-179.ec2.internal>
2022-03-08 13:06:11 +0000

    予約ボタン実装

--- app/controllers/customer/reservations_controller.rb
+++ app/controllers/customer/reservations_controller.rb
@@ -1,9 +1,35 @@
 class Customer::ReservationsController < ApplicationController
+  before_action :authenticate_customer!
+
   def index
     @reservations = Reservation.all.where("day >= ?", Date.current).where("day < ?", Date.current >> 3).order(day: :desc)
+  end
+
+  def new
+    @reservation = Reservation.new
+    @day = params[:day]
+    @time = params[:time]
+    @start_time = DateTime.parse(@day + " " + @time + " " + "JST")
   end
 
   def show
     @reservation = Reservation.find(params[:id])
   end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.customer_id = current_customer.id
+    if @reservation.save
+      redirect_to customer_reservation_path @reservation.id
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:day, :time, :customer_id, :start_time)
+  end
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,18 @@
 class Reservation < ApplicationRecord
+  belongs_to :customer
+
+  def self.reservations_after_three_month
+    # 今日から3ヶ月先までのデータを取得
+    reservations = Reservation.all.where("day >= ?", Date.current).where("day < ?", Date.current >> 3).order(day: :desc)
+    # 配列を作成し、データを格納
+    # DBアクセスを減らすために必要なデータを配列にデータを突っ込んでます
+    reservation_data = []
+    reservations.each do |reservation|
+      reservations_hash = {}
+      reservations_hash.merge!(day: reservation.day.strftime("%Y-%m-%d"), time: reservation.time)
+      reservation_data.push(reservations_hash)
+    end
+    reservation_data
+  end
+
 end
