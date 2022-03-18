a98de95d18b28bebff6b6aa4ee459c4347fcbc05
EC2 Default User <ec2-user@ip-172-31-0-179.ec2.internal>
2022-03-18 11:29:49 +0000

    customer/customer編集ボタン修正中

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -9,6 +9,23 @@ class Customer::CustomersController < ApplicationController
   end
 
   def edit
-    @customer = Customer.find(params[:id])
+    @customer = Customer.find(current_customer.id)
   end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:notice] = "You have updated  successfully."
+      redirect_to customer_customer_path(current_customer)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :kana_family_name, :kana_first_name, :postal_code, :address, :tel, :email, :is_deleted)
+  end
+
 end
