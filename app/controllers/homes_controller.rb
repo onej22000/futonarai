11ac5842f5993254558026de5cd89b18239671df
onej22000 <onej22000@gmail.com>
2022-03-26 10:26:40 +0000

    機器稼働状況実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,25 @@
 class HomesController < ApplicationController
   def top
+    require 'net/http'    # net/httpライブラリを読み込む
 
+    uri = "https://futonmaki.jp/export/kusatsu.txt"
+    @response = Net::HTTP.get_response(URI.parse(uri))
+    @response.body = @response.body.encode(Encoding::UTF_8, Encoding::Shift_JIS)
+    @result = []
+    @response.body.split.each_with_index do | data, i |
+      if i > 3
+        ar = data.split(',')
+        if ar[3] == '空き'
+          @result << ar
+        end
+        if ar[3] == '運転'
+          @result << ar
+        end
+        if ar[3] == '異常'
+          @result << ar
+        end
+      end
+    end
   end
 
   def about; end
